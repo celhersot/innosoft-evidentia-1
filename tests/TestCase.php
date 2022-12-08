77abdc8938456c46eda7bc075f8ed575ac4f3f40
cardelbec <80210617+cardelbec@users.noreply.github.com>
2022-12-08 13:40:28 +0100

    Update TestCase.php

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -9,37 +9,4 @@ use Throwable;
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
-
-    protected bool $init = true;
-
-    protected function init() : void
-    {
-        if($this->init){
-            config(['database.connections.instance' => [
-                'driver' => 'mysql',
-                'host' => env('DB_HOST'),
-                'database' => env('DB_DATABASE'),
-                'port' => env('DB_PORT'),
-                'username' => env('DB_USERNAME'),
-                'password' => env('DB_PASSWORD'),
-                'charset' => 'utf8mb4',
-                'collation' => 'utf8mb4_unicode_ci',
-                'engine' => 'InnoDB',
-            ]]);
-
-            config(['database.default' => 'instance']);
-            $this->init = false;
-        }
-    }
-
-    /**
-     * @throws Throwable
-     */
-    protected function login($email = 'alumno1@alumno1.com', $password = 'alumno1')
-    {
-        return $this->postJson('/api/21/v1/auth/login', [
-            'email' => $email,
-            'password' => $password
-        ])->decodeResponseJson()['token'];
-    }
 }
